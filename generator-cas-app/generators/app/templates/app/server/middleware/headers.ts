1ef777b627e96a26e6a595804dd09194722cccf6
Matthieu Foucault <matthieu@button.is>
2021-10-18 16:47:44 -0700

    chore: only enable csp in production build

--- generator-cas-app/generators/app/templates/app/server/middleware/headers.ts
+++ generator-cas-app/generators/app/templates/app/server/middleware/headers.ts
@@ -1,13 +1,14 @@
 import helmet from "helmet";
 
 const headersMiddleware = () => {
-  const helmetMiddleware = helmet();
+  const helmetMiddleware = helmet({
+    contentSecurityPolicy: process.env.NODE_ENV === "production",
+  });
   return (req, res, next) => {
     // Tell search + crawlers not to index non-production environments:
     if (!process.env.NAMESPACE || !process.env.NAMESPACE.endsWith("-prod")) {
       res.append("X-Robots-Tag", "noindex, noimageindex, nofollow, noarchive");
     }
-
     helmetMiddleware(req, res, next);
   };
 };
